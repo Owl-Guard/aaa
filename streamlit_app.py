6bbdf9bfc5dfcf17ec576958966287bc7169d65c
Owl-Guard <153059994+Owl-Guard@users.noreply.github.com>
2023-12-16 04:40:34 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,48 +4,48 @@ import numpy as np
 from skimage import measure
 from PIL import Image
 
-# Function to process the image and count colonies
+# 画像を処理して細菌のコロニーを数える関数
 def count_colonies(image):
-    # Convert the image to grayscale
+    # 画像をグレースケールに変換
     gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
 
-    # Thresholding the image to a binary image
+    # 画像を二値画像に閾値処理
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    # Perform a series of erosions and dilations to remove any small blobs of noise from the image
+    # 小さなノイズを取り除くために膨張と収縮を行う
     thresh = cv2.erode(thresh, None, iterations=2)
     thresh = cv2.dilate(thresh, None, iterations=4)
 
-    # Perform connected component analysis on the thresholded image
+    # 二値画像に対して連結成分の解析を実行
     labels = measure.label(thresh, connectivity=2, background=0)
 
-    # Find the contours of the masked regions
+    # マスク領域の輪郭を見つける
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-    # Draw red circles around the colonies
+    # コロニーの周りに赤い円を描画
     for c in cnts:
         ((x, y), r) = cv2.minEnclosingCircle(c)
         cv2.circle(np.array(image), (int(x), int(y)), int(r), (255, 0, 0), 2)
     
     return image, len(cnts)
 
-# Streamlit application
-st.title('Bacterial Colony Counter')
+# Streamlitアプリケーションの開始
+st.title('細菌コロニーカウンター')
 
-# File uploader allows user to add their own image
-uploaded_file = st.file_uploader("Upload an image of bacterial colonies", type=["jpg", "jpeg", "png"])
+# ユーザーが自分の画像をアップロードできるようにする
+uploaded_file = st.file_uploader("細菌コロニーが写った画像をアップロードしてください", type=["jpg", "jpeg", "png"])
 
-# If the user uploaded an image, display it and process it
+# ユーザーが画像をアップロードした場合に表示して処理する
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='アップロードされた画像', use_column_width=True)
     
-    # Process the image and count the colonies
+    # 画像を処理してコロニーの数を数える
     processed_image, colony_count = count_colonies(image)
     
-    # Display the processed image and the count of colonies
-    st.image(processed_image, caption='Processed Image with Counted Colonies', use_column_width=True)
-    st.write(f'Number of colonies detected: {colony_count}')
+    # 処理された画像とコロニーの数を表示
+    st.image(processed_image, caption='コロニーを数えた処理画像', use_column_width=True)
+    st.write(f'検出されたコロニーの数: {colony_count}')
 
-# Run this with `streamlit run your_script.py` in your terminal
+# これを実行するには、ターミナルで `streamlit run your_script.py` コマンドを使用します
